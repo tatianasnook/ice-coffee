b6d7f7a6e62f39a7cd06d5d9eb10ba4cc1d86629
tatianasnook <tatsnook@gmail.com>
2024-02-19 11:25:57 -0600

    added location section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-  const card = document.querySelector('#coffee-card');
+  const productContainer = document.querySelector('#product-container');
 
   async function getCoffee() {
     const results = await fetch('https://api.sampleapis.com/coffee/iced');
@@ -10,15 +10,20 @@
   }
 
   function showData(data) {
-    card.innerHTML = "";
+    productContainer.innerHTML = "";
 
     data.forEach(item => {
-      const titleElement = document.createElement('p');
+      const productCard = document.createElement('div');
+      const titleElement = document.createElement('h3');
       const description = document.createElement('p');
+
       titleElement.innerHTML = item.title;
       description.innerHTML = item.description;
-      card.appendChild(titleElement);
-      card.appendChild(description);
+      productCard.classList.add('product-card')
+
+      productContainer.appendChild(productCard);
+      productCard.appendChild(titleElement);
+      productCard.appendChild(description);
     });
   }
 
