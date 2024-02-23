41124c9f3292352b9105ef4e2b09a03727b4444d
tatianasnook <tatsnook@gmail.com>
2024-02-22 20:19:49 -0600

    added error handling for fetch operation

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+
 (function() {
   const productContainer = document.querySelector('#product-container');
 
-  async function getCoffee() {
-    const results = await fetch('https://api.sampleapis.com/coffee/iced');
-    const data = await results.json();
-    console.log(data);
+    // Function to fetch coffee data from the API
 
-    showData(data);
+  async function getCoffee() {
+    try {
+      const results = await fetch('https://api.sampleapis.com/coffee/iced');
+
+      if (!results.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const data = await results.json();
+      showData(data);
+
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+      productContainer.innerHTML = '<p>Error fetching data. Please try again later.</p>';
+    }
   }
 
+  // Function to display the fetched data in the product container
+  
   function showData(data) {
     productContainer.innerHTML = "";
 
@@ -30,6 +44,7 @@
   getCoffee();
 })();
 
+// Scrolling for navigation links
 let links = document.querySelectorAll('.nav-link')
 for(let i = 0; i < links.length; i++){
   links[i].onclick = function(){
